1161169d084a3440b1f5d841877f22896f4c1172
Tatiane Stivelman <tati.s.stiv@gmail.com>
2019-07-03 17:56:40 -0300

    muda a estrutura inicial do ex 1 do lab_06

--- Lab_06/TypeScript_Lab06_1/src/api/livros.rota.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-import * as controlador from './livros.controlador'
-
-export const router = Router();
-export const path = '/livros';
-
-router.get('', controlador.getLivros);
-router.get('/:id', controlador.getLivro);
--- Lab_06/TypeScript_Lab06_1/src/index.ts
+++ Lab_06/TypeScript_Lab06_1/src/index.ts
@@ -1,18 +1,65 @@
-import app from './app';
-import {connect} from 'mongoose';
+import fetch, {Response, Request} from 'node-fetch';
 
-(async() => {
-    try{
-        //Conectar ao MongoDB
-        const servidorMongoDB = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`;
-        await connect(servidorMongoDB, {useNewUrlParser:true});
+interface Post {
+    id?:number,
+    userId:number,
+    title:string,
+    body:string
+}
+
+async function Main(){
+    
+    const urlBase = 'https://jsonplaceholder.typicode.com';
+    
+    try {
+        /*//Realizar um GET
+        let resposta: Response = await fetch(`${urlBase}/posts`);
+        if (resposta.ok){
+            let posts: Post[] = await resposta.json();
+            posts.forEach(p => console.log(p.title));
+        } else {
+            console.log(`GET sem sucesso: ${resposta.status} ${resposta.statusText}`);
+        }*/
         
-        //Iniciar Express
-        app.listen(app.get('port'), () => {
-            console.log(`Express executando em http://localhost:${app.get('port')} no modo ${app.get('env')}`);
+        /*//GET de um post especifico
+        let resposta: Response = await fetch(`${urlBase}/posts/1`);
+        if (resposta.ok){
+            let post: Post = await resposta.json();
+            console.log(post);
+        } else {
+            console.log(`GET sem sucesso: ${resposta.status} ${resposta.statusText}`);
+        }*/
+
+        /*//Realizar um POST
+        let post: Post = {
+            title: "aiufeeoifje",
+            body: "dufohsufhiuf",
+            userId: 1
+        };
+        let resposta: Response = await fetch(`${urlBase}/posts`,{
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(post)
         });
+        if (resposta.ok){
+            let post: Post = await resposta.json();
+            console.log(post);
+        } else {
+            console.log(`GET sem sucesso: ${resposta.status} ${resposta.statusText}`);
+        }*/
+
+        //Realizar um DELETE
+        let resposta: Response = await fetch(`${urlBase}/posts/1`, {
+            method: 'DELETE'
+        });
+        console.log(`${resposta.status} ${resposta.statusText}`);
+
+
+    } catch(erro){
+        console.log(`Erro: ${erro}`);
     }
-    catch(error){
-        console.log(`Erro: ${error}`);
-    }
-})();
+}
+
+Main();
